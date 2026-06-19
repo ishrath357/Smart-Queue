cc128bab460e1eb4479daaeed81f5fb956bd26d1
ishrath357 <ishrathshaik9966@gmail.com>
2026-06-19 22:08:44 +0530

    Frontend connected to backend

--- src/pages/Analytics.js
+++ src/pages/Analytics.js
@@ -1,5 +1,7 @@
 import Navbar from "./Navbar";
 import "./Analytics.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import {
   Chart as ChartJS,
@@ -18,6 +20,37 @@ ChartJS.register(
 
 function Analytics() {
 
+  const [stats, setStats] = useState({
+    waitingUsers: 0,
+    servingUsers: 0,
+    servedUsers: 0
+  });
+
+  useEffect(() => {
+
+    const loadData = () => {
+      axios
+        .get(
+          "https://smart-queue-backend-dpow.onrender.com/dashboard"
+        )
+        .then((res) => {
+          setStats(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+    loadData();
+
+    const interval = setInterval(() => {
+      loadData();
+    }, 3000);
+
+    return () => clearInterval(interval);
+
+  }, []);
+
   const data = {
     labels: [
       "Waiting",
@@ -26,12 +59,17 @@ function Analytics() {
     ],
     datasets: [
       {
-        data: [5, 2, 8],
+        data: [
+          stats.waitingUsers,
+          stats.servingUsers,
+          stats.servedUsers
+        ],
         backgroundColor: [
           "#3b82f6",
           "#22c55e",
           "#64748b"
-        ]
+        ],
+        borderWidth: 2
       }
     ]
   };
@@ -42,7 +80,7 @@ function Analytics() {
 
       <div className="analytics-container">
 
-        <h2>Queue Analytics</h2>
+        <h2>📊 SmartQueue Analytics</h2>
 
         <div className="chart-box">
           <Pie data={data} />
@@ -51,18 +89,18 @@ function Analytics() {
         <div className="stats">
 
           <div className="stat-card">
-            <h3>Waiting</h3>
-            <p>5</p>
+            <h3>⏳ Waiting</h3>
+            <p>{stats.waitingUsers}</p>
           </div>
 
           <div className="stat-card">
-            <h3>Serving</h3>
-            <p>2</p>
+            <h3>🟢 Serving</h3>
+            <p>{stats.servingUsers}</p>
           </div>
 
           <div className="stat-card">
-            <h3>Served</h3>
-            <p>8</p>
+            <h3>✅ Served</h3>
+            <p>{stats.servedUsers}</p>
           </div>
 
         </div>
